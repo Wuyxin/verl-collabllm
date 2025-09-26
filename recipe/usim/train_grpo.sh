9cb49700ca73957fd1bede2af61cf0291a15944c
Wuyxin <wuyxinsh@gmail.com>
2025-09-26 14:30:06 -0700

    update to 7b

--- recipe/usim/train_grpo.sh
+++ recipe/usim/train_grpo.sh
@@ -5,9 +5,8 @@ export VLLM_USE_V1=1
 # If you are using vllm<=0.6.3, you might need to set the following environment variable to avoid bugs:
 # export VLLM_ATTENTION_BACKEND=XFORMERS
 
-
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export NUM_GPUS=8
+export NUM_GPUS=4
 
 EXP_NAME=qwen2_5_stage1
 DATA_PATH=/dfs/project/kgrlm/common/llm_twin/processed_data/reddit_debug
@@ -31,8 +30,8 @@ export VERL_CACHE_DIR="$NEW_HF_CACHE/verl-cache"
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
+    trainer.val_before_train=False \
     algorithm.use_kl_in_reward=False \
-    reward_model.enable=False \
     reward_model.reward_manager=usim \
     custom_reward_function.path="$VERL_PATH/recipe/usim/reward.py" \
     custom_reward_function.name="compute_reward" \
@@ -46,17 +45,18 @@ python3 -m verl.trainer.main_ppo \
     data.train_batch_size=16 \
     data.val_batch_size=256 \
     +data.kwargs.chat_template_path="$VERL_PATH/recipe/usim/chat_templates/qwen_multi_role_template.jinja"\
-    data.max_prompt_length=8192 \
+    data.max_prompt_length=4096 \
     data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
     +actor_rollout_ref.kwargs.custom_chat_template="$VERL_PATH/recipe/usim/chat_templates/qwen_multi_role_template.jinja" \
     actor_rollout_ref.model.path=$MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
     actor_rollout_ref.rollout.load_format=safetensors \
-    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -66,9 +66,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.dtype=bfloat16 \
     actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.n=8 \
     '+actor_rollout_ref.rollout.stop="<response>"' \
     actor_rollout_ref.rollout.temperature=1.0 \
@@ -89,28 +90,30 @@ python3 -m verl.trainer.main_ppo \
     trainer.save_freq=100 \
     trainer.test_freq=10 \
     trainer.default_hdfs_dir=null \
-    trainer.val_before_train=True \
-    +trainer.hf_hub.enable=True \
-    +trainer.hf_hub.repo_id="snap-stanford/grpo_model" \
-    +trainer.hf_hub.private=True \
-    +trainer.hf_hub.branch="main" \
     trainer.total_epochs=30 $@
 
-    # either set HUGGINGFACE_HUB_TOKEN in env or do trainer.hf_hub.token=
+    # +trainer.hf_hub.enable=True \
+    # +trainer.hf_hub.repo_id="snap-stanford/grpo_model" \
+    # +trainer.hf_hub.private=True \
+    # +trainer.hf_hub.branch="main" \
 
-    # +trainer.train_given_tag=False \
-    # +trainer.hf_hub.token="" \
+#############################################
+# Optional / Commented Config
+#############################################
+# Either set HUGGINGFACE_HUB_TOKEN in env or do trainer.hf_hub.token=
+# +trainer.hf_hub.token="" 
 
-    # actor_rollout_ref.model.path="/dfs/project/kgrlm/common/llm_twin/models/Qwen2.5-14B-Instruct" \
-    # actor_rollout_ref.model.lora_rank=16 \
-    # actor_rollout_ref.model.lora_alpha=16 \
-    #trainer.resume_mode=resume_path \
-    #trainer.resume_from_path="$RESUME_PATH" \
-        #actor_rollout_ref.model.path="$RESUME_PATH" \
-    #+actor_rollout_ref.ref.model.path="$RESUME_PATH" \
-    # '+actor_rollout_ref.rollout.stop=["</response>"]' \
+# actor_rollout_ref.model.path="/dfs/project/kgrlm/common/llm_twin/models/Qwen2.5-7B-Instruct" \
+# actor_rollout_ref.model.lora_rank=16 \
+# actor_rollout_ref.model.lora_alpha=16 \
 
-    # +data.tags.tag_path="/dfs/project/kgrlm/common/llm_twin/data/reddit_debug_verl/index_tags.jsonl" \
-    # +data.tags.index_col="index" \
-    # +data.tags.tag_col="tags" \
-    # +data.tags.wrap_with_markers=True \
+# trainer.resume_mode=resume_path \
+# trainer.resume_from_path="$RESUME_PATH" \
+# actor_rollout_ref.model.path="$RESUME_PATH" \
+# +actor_rollout_ref.ref.model.path="$RESUME_PATH" \
+# '+actor_rollout_ref.rollout.stop=["</response>"]' \
+
+# +data.tags.tag_path="/dfs/project/kgrlm/common/llm_twin/data/reddit_debug_verl/index_tags.jsonl" \
+# +data.tags.index_col="index" \
+# +data.tags.tag_col="tags" \
+# +data.tags.wrap_with_markers=True \
